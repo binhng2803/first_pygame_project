e92d18f60aae2292eb87eee2ac41c6e7de458fa1
Nguyễn Gia Bình <binhnghe187025@fpt.edu.vn>
2023-03-24 23:34:27 +0700

    create the window of game

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 import pygame
+
+# create a window of game
+WIDTH, HEIGHT = 900, 600
+WIN = pygame.display.set_mode(((WIDTH, HEIGHT)))
+
+def main():
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                
+    pygame.quit()
+    
+if __name__ == '__main__':
+    main()
