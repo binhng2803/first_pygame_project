1dbf9e7defcdac548ff05514d9209c04a77ec327
Nguyễn Gia Bình <binhnghe187025@fpt.edu.vn>
2023-04-10 01:25:23 +0700

    display spaceships

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import os
 
 # create a window of game
 WIDTH, HEIGHT = 900, 600
@@ -7,8 +8,23 @@ pygame.display.set_caption('My first pygame project!')
 COLOR = '#8776FF'
 FPS = 60
 
+SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 70, 50
+
+red_spaceship_image = pygame.image.load(
+    os.path.join('Assets', 'spaceship_red.png')) 
+red_spaceship = pygame.transform.rotate(pygame.transform.scale(
+    red_spaceship_image,(SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 90)
+
+yellow_spaceship_image = pygame.image.load(
+    os.path.join('Assets', 'spaceship_yellow.png')) 
+yellow_spaceship = pygame.transform.rotate(pygame.transform.scale(
+    yellow_spaceship_image,(SPACESHIP_WIDTH, SPACESHIP_HEIGHT)), 270)
+
+
 def draw_window():
     WIN.fill(COLOR)
+    WIN.blit(red_spaceship, (30, 265))
+    WIN.blit(yellow_spaceship, (820, 265))
     pygame.display.update()
 
 def main():
